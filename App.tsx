b0589ddeeeff185b59eeeb4457cd086126a792f7
Saurabh Ravte <saurabh.ravte@gmail.com>
2026-05-21 16:25:18 +0530

    bug: root app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import "react-native-gesture-handler";
 import React from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Navigation from "./src/navigation";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <AuthProvider>
       <Navigation />
-    </GestureHandlerRootView>
+    </AuthProvider>
   );
 }
